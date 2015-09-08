33d93c85d75ce9bd41050886b8f5a2c03417f774
Andrew Ewing <drew@phenocode.com>
2015-09-07 23:17:19 -0700

    Updated registry class

--- Registry.hh
+++ Registry.hh
@@ -5,15 +5,30 @@ class Registry {
   public function __construct(KeyedTraversable<mixed,mixed> $entries) {
     $this->entries = new Map($entries);
   }
-  public function add(mixed $key, mixed $value) : void {
+  /**
+   * Set a registry value
+   */
+  public function set(mixed $key, mixed $value) : void {
     $this->entries->set($key, $value);
   }
+  /**
+   * Add a registry value (alias of set)
+   */
+  public function add(mixed $key, mixed $value) : void {
+    $this->set($key, $value);
+  }
+  /**
+   * Get a registry value
+   */
   public function get(mixed $key, mixed $default=false) : mixed {
     if($this->entries->contains($key)) {
       return $this->entries->at($key);
     }
     return $default;
   }
+  /**
+   * Get the map of registry values
+   */
   public function map() : Map<mixed,mixed> {
     return $this->entries;
   }
